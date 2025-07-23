f547433ae5b2f2e5c769ef250e86b151a1fec6b0
abhijeet32 <kumarabhijeetips@gmail.com>
2025-07-23 16:32:46 +0530

    Added needs keyword2

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install cowsay -y
-cowsay -f cow "I am a COW... MOOO" >> cow.txt
-grep -i "COW" cow.txt
-cat cow.txt
+cowsay -f dragon "I have a DRAGON... DRACARYS" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
 ls -ltra
